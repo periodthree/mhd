2092e074e16b92728b05944032c4f4e0f665d111
Jay Barry <jay@period-three.com>
2015-10-21 14:36:46 -0400

    Add Info to Footer (name, social media links)

--- laravel/app/views/inc/footer.blade.php
+++ laravel/app/views/inc/footer.blade.php
@@ -1,16 +1,16 @@
 <div class="container footer">
     <div class="row">
         <div class="col-md-6">
-           <p class="copyright">&copy; <?php echo date("Y"); ?> {{ Config::get('site.name')}}</p>
+           <p class="copyright">&copy; <?php echo date("Y"); ?> Cregger Company, Inc.</p>
         </div>
 
         <div class="col-md-6">
             <div class="pull-right">
-                <a href="https://www.facebook.com/mcclureair">
+                <a href="https://www.facebook.com/creggercompany">
                     <svg class="icon icon-facebook2"><use xlink:href="#icon-facebook2"></use></svg><span class="mls sr-only sr-only-focusable"> Facebook</span>
                 </a>
 
-                <a href="https://twitter.com/mcclure_air">
+                <a href="https://twitter.com/CreggerCompany">
                     <svg class="icon icon-twitter"><use xlink:href="#icon-twitter"></use></svg><span class="mls sr-only sr-only-focusable"> Twitter</span>
                 </a>
             </div>
