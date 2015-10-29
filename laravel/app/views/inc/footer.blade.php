0daff86bfb84ed287509074d0cd77f463b9afd66
Jay Barry <jay@period-three.com>
2015-10-29 11:19:27 -0400

    Add Footer Logos

--- laravel/app/views/inc/footer.blade.php
+++ laravel/app/views/inc/footer.blade.php
@@ -1,10 +1,18 @@
 <div class="container footer">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-3">
            <p class="copyright">&copy; <?php echo date("Y"); ?> Cregger Company, Inc.</p>
         </div>
 
         <div class="col-md-6">
+           <div class="footer-logos">
+                <img src="{{ asset('assets/img/goodman-logo-wht-opt.png') }}" alt="Goodman Air Conditioning &amp; Heating">
+                <img src="{{ asset('assets/img/cregger-logo-wht-opt.png') }}" alt="Cregger Company">
+                <img src="{{ asset('assets/img/mcclure-logo-wht-opt.png') }}" alt="McClure Air">
+            </div>
+        </div>
+
+        <div class="col-md-3">
             <div class="pull-right">
                 <a href="https://www.facebook.com/creggercompany">
                     <svg class="icon icon-facebook2"><use xlink:href="#icon-facebook2"></use></svg><span class="mls sr-only sr-only-focusable"> Facebook</span>
